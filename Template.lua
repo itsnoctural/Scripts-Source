66d3c1eab061704bffc5e524e775af6d9afad501
itsnoctural <nocturalthe@gmail.com>
2023-09-10 13:54:11 +0300

    add Anti-AFK

--- Template.lua
+++ Template.lua
@@ -1,5 +1,6 @@
 local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
+local VirtualUser = game:GetService("VirtualUser")
 
 local LocalPlayer = Players.LocalPlayer
 
@@ -29,4 +30,10 @@ Window:Button("YouTube: EsohaSL", function()
             setclipboard("https://youtube.com/@esohasl")
         end)
    end)
+end)
+
+LocalPlayer.Idled:connect(function()
+    VirtualUser:Button2Down(Vector2.new(0,0), Workspace.CurrentCamera.CFrame);
+    task.wait()
+    VirtualUser:Button2Up(Vector2.new(0,0), Workspace.CurrentCamera.CFrame);
 end)
